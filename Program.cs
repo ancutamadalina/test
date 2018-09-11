93a507bca65a78369df6c1a0be8c838756f05bd9
ancutamadalina <35922657+ancutamadalina@users.noreply.github.com>
2018-09-11 16:42:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetParams("1", "2", "3");
+
+            testarray(new string[] { "1", "2", "3" });
+            Console.ReadLine();
+        }
+
+        static void GetParams (params string[] parameters)
+        {
+            foreach (var t1 in parameters)
+            {
+                Console.WriteLine(t1);
+            }
+
+            List<string> listOfParams = new List<string>();
+            foreach (var t1 in parameters)
+            {
+                listOfParams.Add(t1);
+            }
+
+            listOfParams.ToArray();
+          
+
+            foreach (var t2 in listOfParams)
+            {
+                Console.WriteLine("new p2 = " + t2);
+            }
+        }
+
+        static void testarray(string[] str)
+        {
+            foreach (var t2 in str)
+            {
+                Console.WriteLine("new p3 = " + t2);
+            }
+        }
+    }
+}
